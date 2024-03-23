7bd7b23506f755d2deef15b1837d3a2d4f8318c0
jhdresch <jhdresch@gmail.com>
2024-03-23 18:20:34 -0300

    criando meu primeiro projeto react entendendo componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
+
+
+class Equipe2 extends Component {
+  render() {
+    return (
+      <div>
+        <h1> usando classe</h1>
+        <h2>meu cardo é {this.props.cargo}</h2>
+      </div>
+    );
+  }
+}
+
+
+const BemVindo = (props) => <h2>Bem Vindo {props.nome}</h2>
+
+const BemVindo2 = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Bem Vindo 2 {props.nome1}</h2>
+      <h1>Bem Vindo 2 {props.nome2}</h1>
+    </div>
+  )
+}
+
+const Sobre = (props) => {
+  return (
+    <div>
+      <h3>Meu nome é {props.nome}.  </h3>
+      <h4>Minha idade é {props.idade} anos.</h4>
+      <h4>Cargo: {props.cargo}.</h4>
     </div>
   );
 }
+
+const Equipe = (props) => {
+  return (
+    <Sobre nome={props.nome} idade={props.idade} cargo={props.cargo} />
+  );
+}
+
+function App() {
+  return (
+
+    <><div>
+      <h1>Ola Mundo</h1>
+      <BemVindo nome='Jair' />
+      <BemVindo2 nome1='Maria' nome2='Jose' />
+    </div>
+      <div>
+        <h1>Conheça nossa equipe</h1>
+        <Equipe nome="Jair" idade="40" cargo="TL" />
+
+      </div></>
+
+
+  );
+
+}
+
+
 
 export default App;
